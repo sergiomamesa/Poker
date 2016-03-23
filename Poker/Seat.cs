193d1846120b95fdabb4d5c98b74649d0cb9e0c5
Sergio Magaña <sergiotem@gmail.com>
2016-03-23 09:27:48 +0100

    readded file seat.cs

---
+++ Poker/Seat.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Poker
+{
+    public class Seat
+    {
+        public int SeatNumber { get; set; }
+        public Player Player { get; set; }
+
+        public Seat(int seatNumber)
+        {
+            SeatNumber = seatNumber;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Player == null)
+                return true;
+
+            return false;
+        }
+    }
+
+    public class SeatsList : List<Seat>
+    {
+        public new Seat this[int index] { get { return GetSeat(index); } }
+
+        private Seat GetSeat(int index)
+        {
+            return this.ElementAt(index - 1);
+        }
+
+        internal Seat GetFreeSeat()
+        {
+            if (IsNoneEmpty())
+                throw new Exception("Sorry, not empty seat found");
+
+            Seat seat = this.First(i => i.IsEmpty());
+            
+            return seat;
+        }
+
+        internal static SeatsList GenerateEmptySeats(int MaxNumberPlayers)
+        {
+            var result = new SeatsList();
+
+            for (int i = 1; i <= MaxNumberPlayers; i++)
+            {
+                result.Add(new Seat(i));
+            }
+
+            return result;
+        }
+
+        public bool IsAnyEmpty()
+        {
+            return this.Any(i => i.IsEmpty());
+        }
+
+        public bool IsNoneEmpty()
+        {
+            return !IsAnyEmpty();
+        }
+    }
+}
