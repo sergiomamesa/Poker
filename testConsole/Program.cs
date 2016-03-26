92e597da274d44004578b9aeecb983a6e57c3c85
Manu <manurosa11@gmail.com>
2016-03-26 15:38:04 +0100

    Update prefs and other stuff

--- testConsole/Program.cs
+++ testConsole/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
 using Poker;
-namespace MonoConsole
+
+namespace testConsole
 {
 	class MainClass
 	{
 		public static void Main (string[] args)
 		{
-			Table table = new Table(5);
+//			Console.WriteLine ("Hello World!");
+
+			Table table = new Table (4);
 			table.AddPlayer(new Player() { Name = "Player1" });
 			table.AddPlayer(new Player() { Name = "Player2" });
 			table.AddPlayer(new Player() { Name = "Player3" });
+
 			table.StartGame ();
-			bool expected = table.Seats [1].Player.Hand.LeftCard.Equals (null);
-
-			Console.WriteLine (expected.ToString());
-
-			Console.WriteLine ("Success!");
+			Console.WriteLine ("success!!");
 			Console.ReadLine ();
+
 		}
 	}
 }
